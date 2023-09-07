98c961318942c823e7b8c65726f69430edcc5020
Phat <phatmap234@gmail.com>
2023-09-08 01:11:11 +0700

    add algorithm

---
+++ src/components/Algorithm.js
@@ -0,0 +1,16 @@
+
+export const Linear = (n) => {
+  const result = [];
+  for (let index = 0; index < n; index++) {
+    result.push(index);
+  }
+    return result;
+};
+
+export const Red = (n) => {
+  const result = [];
+  for (let index = 0; index < n; index++) {
+    result.push(index);
+  }
+};
+
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,37 +1,60 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "../styles/Board.css";
+import { Linear, Red } from "./Algorithm";
 
 const Board = () => {
   const [array, setArray] = useState([]);
-  const element = "board-item set";
+  const [isVisible, setIsVisible] = useState(false);
+  const [n, setN] = useState(0);
+  const inputRef = useRef(null);
+  const [result, setResult] = useState([]);
+
+  function handleInput(event) {
+    if (event.key == "Enter") {
+      setInitValue();
+    }
+  }
+
+  function setInitValue() {
+    const inputValue = inputRef.current.value;
+    setN(inputValue);
+    setIsVisible(true);
+    inputRef.current.value = "";
+  }
 
   function elementFactory() {
     const newArray = [];
-    for (let index = 0; index < 64; index++) {
-      newArray.push(element);
+    for (let index = 0; index < n; index++) {
+      newArray.push("board-item set");
     }
     setArray(newArray);
   }
 
-  function paintBox() {
+  function algorithmRun() {
     const newArray = [...array];
-    newArray[randomNumber(63,0)] = "board-item unset";
-    setArray(newArray);
-  }
-
-  function randomNumber(max, min) {
-    const random = Math.random();
-    const randomNumber = Math.floor(random * (max - min + 1) + min);
-    return randomNumber;
+    result.forEach((element, index) => {
+      setTimeout(() => {
+        newArray[element] = "board-item unset";
+        setArray([...newArray]);
+      }, index * 1000);
+    });
   }
 
   useEffect(() => {
+    setResult([...Linear(n)]);
     elementFactory();
-  }, []);
+  }, [n]);
 
   return (
-    <div>
-      <button onClick={paintBox}>Start</button>
+    <>
+      <div className="control">
+        <input
+          className="control-input"
+          ref={inputRef}
+          onKeyDown={handleInput}
+        ></input>
+        {isVisible && <button onClick={algorithmRun}>Start</button>}
+      </div>
       <div className="board-container">
         {array.map((element, index) => (
           <p key={index} className={element}>
@@ -39,7 +62,7 @@ const Board = () => {
           </p>
         ))}
       </div>
-    </div>
+    </>
   );
 };
 
